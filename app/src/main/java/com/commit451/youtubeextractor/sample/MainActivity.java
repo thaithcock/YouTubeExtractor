287760218364d4484a19095be7f7d829048de136
John Carlson <jawnnypoo@gmail.com>
2017-08-11 09:26:26 -0500

    Latest tools and things

--- app/src/main/java/com/commit451/youtubeextractor/sample/MainActivity.java
+++ app/src/main/java/com/commit451/youtubeextractor/sample/MainActivity.java
@@ -10,20 +10,14 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.commit451.youtubeextractor.YouTubeExtractionResult;
 import com.commit451.youtubeextractor.YouTubeExtractor;
-import com.devbrackets.android.exomedia.core.video.scale.ScaleType;
-import com.devbrackets.android.exomedia.listener.OnCompletionListener;
 import com.devbrackets.android.exomedia.listener.OnErrorListener;
 import com.devbrackets.android.exomedia.listener.OnPreparedListener;
-import com.devbrackets.android.exomedia.ui.widget.EMVideoView;
+import com.devbrackets.android.exomedia.ui.widget.VideoView;
 
 import io.reactivex.SingleObserver;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,49 +27,9 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private TextView description;
-    private EMVideoView videoView;
+    private VideoView videoView;
 
     private int savedPosition;
-
-    private Callback<YouTubeExtractionResult> mExtractionCallback = new Callback<YouTubeExtractionResult>() {
-        @Override
-        public void onResponse(Call<YouTubeExtractionResult> call, Response<YouTubeExtractionResult> response) {
-            bindVideoResult(response.body());
-        }
-
-        @Override
-        public void onFailure(Call<YouTubeExtractionResult> call, Throwable t) {
-            onError(t);
-        }
-    };
-
-    private OnPreparedListener mOnPreparedListener = new OnPreparedListener() {
-        @Override
-        public void onPrepared() {
-            if (videoView == null) {
-                return;
-            }
-
-            videoView.setVolume(0);
-            videoView.seekTo(savedPosition);
-            savedPosition = 0;
-            videoView.start();
-        }
-    };
-    private OnCompletionListener mOnCompletionListener = new OnCompletionListener() {
-        @Override
-        public void onCompletion() {
-            //I dunno, maybe play it again
-        }
-    };
-    private OnErrorListener mOnErrorListener = new OnErrorListener() {
-        @Override
-        public boolean onError() {
-            //cry I guess
-            Log.d("ERROR", "There was an error. Oh no!");
-            return true;
-        }
-    };
 
     private final YouTubeExtractor extractor = YouTubeExtractor.create();
 
@@ -83,9 +37,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = (ImageView) findViewById(R.id.thumb);
-        videoView = (EMVideoView) findViewById(R.id.video_view);
-        description = (TextView) findViewById(R.id.description);
+        imageView = findViewById(R.id.thumb);
+        videoView = findViewById(R.id.video_view);
+        description = findViewById(R.id.description);
 
         extractor.extract(GRID_YOUTUBE_ID)
                 .subscribeOn(Schedulers.io())
@@ -108,12 +62,31 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState != null) {
             savedPosition = savedInstanceState.getInt(STATE_SAVED_POSITION, 0);
         }
+        videoView.setOnPreparedListener(new OnPreparedListener() {
+            @Override
+            public void onPrepared() {
+                if (videoView == null) {
+                    return;
+                }
+
+                videoView.setVolume(0);
+                videoView.seekTo(savedPosition);
+                savedPosition = 0;
+                videoView.start();
+            }
+        });
+        videoView.setOnErrorListener(new OnErrorListener() {
+            @Override
+            public boolean onError(Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        videoView.setScaleType(ScaleType.CENTER_CROP);
         videoView.seekTo(savedPosition);
         videoView.start();
     }
@@ -134,10 +107,6 @@ public class MainActivity extends AppCompatActivity {
         Glide.with(this)
                 .load(result.getBestAvailableQualityThumbUri())
                 .into(imageView);
-        videoView.setScaleType(ScaleType.CENTER_CROP);
-        videoView.setOnPreparedListener(mOnPreparedListener);
-        videoView.setOnCompletionListener(mOnCompletionListener);
-        videoView.setOnErrorListener(mOnErrorListener);
         videoView.setVideoURI(result.getBestAvailableQualityVideoUri());
     }
 }
