b0c28b8a7dc4f697172df3753dce7774e0c83cc9
John Carlson <jawnnypoo@gmail.com>
2020-01-24 10:14:54 -0600

    Update to decryption

--- youtubeextractor/src/main/java/com/commit451/youtubeextractor/JavaScriptUtil.kt
+++ youtubeextractor/src/main/java/com/commit451/youtubeextractor/JavaScriptUtil.kt
@@ -9,16 +9,20 @@ import org.mozilla.javascript.Function
 internal object JavaScriptUtil {
 
     private const val DECRYPTION_FUNC_NAME = "decrypt"
-    private const val DECRYPTION_SIGNATURE_FUNCTION_REGEX = "(\\w+)\\s*=\\s*function\\((\\w+)\\)\\{\\s*\\2=\\s*\\2\\.split\\(\"\"\\)\\s*;"
+    private const val DECRYPTION_SIGNATURE_FUNCTION_REGEX = "([\\w$]+)\\s*=\\s*function\\((\\w+)\\)\\{\\s*\\2=\\s*\\2\\.split\\(\"\"\\)\\s*;"
+    private const val DECRYPTION_SIGNATURE_FUNCTION_REGEX_2 = "\\b([\\w$]{2})\\s*=\\s*function\\((\\w+)\\)\\{\\s*\\2=\\s*\\2\\.split\\(\"\"\\)\\s*;"
     private const val DECRYPTION_AKAMAIZED_STRING_REGEX = "yt\\.akamaized\\.net/\\)\\s*\\|\\|\\s*.*?\\s*c\\s*&&\\s*d\\.set\\([^,]+\\s*,\\s*([a-zA-Z0-9$]+)\\("
     private const val DECRYPTION_AKAMAIZED_SHORT_STRING_REGEX = "\\bc\\s*&&\\s*d\\.set\\([^,]+\\s*,\\s*([a-zA-Z0-9$]+)\\("
 
+    private val decryptionFunctions = listOf(
+        DECRYPTION_SIGNATURE_FUNCTION_REGEX_2,
+        DECRYPTION_SIGNATURE_FUNCTION_REGEX,
+        DECRYPTION_AKAMAIZED_SHORT_STRING_REGEX,
+        DECRYPTION_AKAMAIZED_STRING_REGEX
+    )
+
     fun loadDecryptionCode(playerCode: String): String {
-        val decryptionFunctionName: String = when {
-            Util.isMatch(DECRYPTION_AKAMAIZED_SHORT_STRING_REGEX, playerCode) -> Util.matchGroup1(DECRYPTION_AKAMAIZED_SHORT_STRING_REGEX, playerCode)
-            Util.isMatch(DECRYPTION_AKAMAIZED_STRING_REGEX, playerCode) -> Util.matchGroup1(DECRYPTION_AKAMAIZED_STRING_REGEX, playerCode)
-            else -> Util.matchGroup1(DECRYPTION_SIGNATURE_FUNCTION_REGEX, playerCode)
-        }
+        val decryptionFunctionName = decryptionFunctionName(playerCode)
         val functionPattern = "(" + decryptionFunctionName.replace("$", "\\$") + "=function\\([a-zA-Z0-9_]+\\)\\{.+?\\})"
         val decryptionFunction = "var " + Util.matchGroup1(functionPattern, playerCode) + ";"
 
@@ -28,6 +32,15 @@ internal object JavaScriptUtil {
         val callerFunction = "function $DECRYPTION_FUNC_NAME(a){return $decryptionFunctionName(a);}"
 
         return helperObject + decryptionFunction + callerFunction
+    }
+
+    private fun decryptionFunctionName(playerCode: String): String {
+        decryptionFunctions.forEach {
+            try {
+                return Util.matchGroup1(it, playerCode)
+            } catch (e: Exception) { }
+        }
+        throw IllegalStateException("Could not find decryption function with any of the patterns. Please open a new issue")
     }
 
     fun decryptSignature(encryptedSig: String, decryptionCode: String): String {
